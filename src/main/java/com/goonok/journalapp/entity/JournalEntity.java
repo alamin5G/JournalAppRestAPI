84617579a0cde3bbd31668e2c82b4c8b02480a3d
alamin5g <21303134@iubat.edu>
2024-04-16 22:05:53 +0600

    adding the lombok and remove the getter setter

--- src/main/java/com/goonok/journalapp/entity/JournalEntity.java
+++ src/main/java/com/goonok/journalapp/entity/JournalEntity.java
@@ -1,6 +1,10 @@
 package com.goonok.journalapp.entity;
 
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data //is equal to getter, setter, toString, and more..
 public class JournalEntity {
 
     @Id
@@ -18,53 +23,5 @@ public class JournalEntity {
 
     private LocalDateTime publishedDate;
 
-    public LocalDateTime getPublishedDate() {
-        return publishedDate;
-    }
 
-    public void setPublishedDate(LocalDateTime publishedDate) {
-        this.publishedDate = publishedDate;
-    }
-
-    public String getJournal() {
-        return journal;
-    }
-
-    public void setJournal(String journal) {
-        this.journal = journal;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    @Override
-    public String toString() {
-        return "JournalEntity{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", journal='" + journal + '\'' +
-                ", notes='" + notes + '\'' +
-                '}';
-    }
 }
